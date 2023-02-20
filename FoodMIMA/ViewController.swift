7256d9684f1c6d9f0e072d31243224222ebfa871
Johnson Olusegun <olusegun.johnson.oj@gmail.com>
2023-02-20 08:58:30 -0500

    Initial Commit

---
+++ FoodMIMA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodMIMA
+//
+//  Created by Johnson Olusegun on 2/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
